dceb4c439e973aecedd3bab83aeeaf3736426c14
Mateusz Bąk <mateusz.bak@x-kom.pl>
2020-09-17 13:52:39 +0200

    Initial Commit

---
+++ Unsecured App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unsecured App
+//
+//  Created by Mateusz Bąk on 17/09/2020.
+//  Copyright © 2020 Mateusz Bąk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
