0110b386b104835ca2b20d551abf6e82c92631b2
Mateusz Bąk <mateusz.bak@x-kom.pl>
2020-09-24 20:59:12 +0200

    Add authentication manager

---
+++ Unsecured App/Managers/Authentication/AuthenticationManager.swift
@@ -0,0 +1,63 @@
+//
+//  AuthenticationManager.swift
+//  Unsecured App
+//
+//  Created by Mateusz Bąk on 24/09/2020.
+//  Copyright © 2020 Mateusz Bąk. All rights reserved.
+//
+
+import Foundation
+
+protocol AuthenticationManagerProtocol {
+    func createSessionWith(username: String, password: String, completion: @escaping (Error?) -> Void)
+}
+
+struct AuthenticationManager: AuthenticationManagerProtocol {
+    
+    private let requestTokenNetworkManager = NetworkManager<AuthenticationService, RequestToken>()
+    private let sessionNetworkManager = NetworkManager<AuthenticationService, Session>()
+    
+}
+
+extension AuthenticationManager {
+    func createSessionWith(username: String, password: String, completion: @escaping (Error?) -> Void) {
+        createRequestToken(username: username, password: password, completion: completion)
+    }
+}
+
+private extension AuthenticationManager {
+    func createRequestToken(username: String, password: String, completion: @escaping (Error?) -> Void) {
+        requestTokenNetworkManager.request(from: .createRequestToken) { result in
+            switch result {
+            case .success(let requestToken):
+                createSessionWithLogin(username: username, password: password, requestToken: requestToken.requestToken, completion: completion)
+            case .failure(let error):
+                completion(error)
+            }
+        }
+    }
+    
+    func createSessionWithLogin(username: String, password: String, requestToken: String, completion: @escaping (Error?) -> Void) {
+        requestTokenNetworkManager.request(from: .createSessionWithLogin(username: username, password: password, requestToken: requestToken)) { result in
+            switch result {
+            case .success(let requestToken):
+                createSession(requestToken: requestToken.requestToken, completion: completion)
+            case .failure(let error):
+                completion(error)
+            }
+        }
+    }
+    
+    func createSession(requestToken: String, completion: @escaping (Error?) -> Void) {
+        sessionNetworkManager.request(from: .createSession(requestToken: requestToken)) { result in
+            switch result {
+            case .success(let session):
+                // TODO: Add saveing session in Keychain
+                print(session)
+                completion(nil)
+            case .failure(let error):
+                completion(error)
+            }
+        }
+    }
+}
