9b5b6fb234885954d43ee6efc711bdcfed279a0c
Mateusz Bąk <mateusz.bak@x-kom.pl>
2020-10-17 14:58:05 +0200

    Add Unit Testes

---
+++ Secured App/Secured App Unit Tests/Secured_App_Unit_Tests.swift
@@ -0,0 +1,20 @@
+//
+//  Secured_App_Unit_Tests.swift
+//  Secured App Unit Tests
+//
+//  Created by Mateusz Bąk on 17/10/2020.
+//  Copyright © 2020 Mateusz Bąk. All rights reserved.
+//
+
+import XCTest
+@testable import Secured_App
+
+final class Secured_App_Unit_Tests: XCTestCase {
+
+    func test_JailbreakDetectionServicePerformance() {
+        measure {
+            let _ = JailbreakDetectionService.isJailbroken()
+        }
+    }
+
+}
